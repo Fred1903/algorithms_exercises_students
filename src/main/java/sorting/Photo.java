75a47666470a5b6e8c1291616cca2d9f7f3d8f28
Friedrich Röben <100136262+Fred1903@users.noreply.github.com>
2025-11-01 17:46:43 +0100

    b

--- src/main/java/sorting/Photo.java
+++ src/main/java/sorting/Photo.java
@@ -36,7 +36,71 @@ public class Photo {
      * @return the sum of the difference between players on the same spot. If 
      *         no arrangement can be found, returns -1
      */
+
+    //Mon code est pas 0(n log(n)) car insertion sort c'est o(n²) !!! arrays.sort c'est ok vu que j ai acces au librairies
+    //et ca utilise dual-pivot quicksort... voir soluce prof mdrr
     public static int canTakePictures(int [] teamA, int [] teamB) {
-         return -2;
+        if(teamA.length != teamB.length)return -1;
+        sort(teamA);
+        System.out.println("Team a : ");
+        showTeam(teamA);
+        sort(teamB);
+        System.out.println("Team b : ");
+        showTeam(teamB);
+
+        return alignTeamsInRow(teamA,teamB);
+    }
+
+    public static void sort(int [] team){
+        for(int i=0; i<team.length;i++){
+            for(int j=i;j>0 && less(team[j],team[j-1]);j--){
+                exch(team,j,j-1);
+            }
+        }
+    }
+
+    public static boolean less(int v, int w){
+        return v<w;
+    }
+
+    public static void exch(int [] team, int i, int j){
+        int t=team[i];
+        team[i]=team[j];
+        team[j]=t;
+    }
+
+    public static void showTeam(int [] team){
+        for (int i=0; i<team.length;i++){
+            System.out.print(team[i]+" ");
+        }
+        System.out.println();
+    }
+
+    private static int alignTeamsInRow(int[] teamA, int[] teamB){
+        int tallestPlayerTeamA = teamA[teamA.length-1];
+        int tallestPlayerTeamB = teamB[teamB.length-1];
+
+        if(tallestPlayerTeamA<tallestPlayerTeamB){
+            return isRowPossible(teamB,teamA);
+        }
+        else{
+            return isRowPossible(teamA,teamB);
+        }
+    }
+
+    private static int isRowPossible(int[] tallTeam, int[] smallTeam){
+        int totalSizeGapTeams=0;
+        for (int i=tallTeam.length-1; i>=0;i--){
+            if(tallTeam[i]<=smallTeam[i]) return -1;
+            totalSizeGapTeams += tallTeam[i]-smallTeam[i];
+        }
+        return totalSizeGapTeams;
+    }
+
+    public static void main(String[] args) {
+        int [] a = {170,182,168,190,165}; //--> 165,168,170,182,190
+        int [] b = {180,172,165,142,165}; //--> 142,165,165,172,180
+
+        System.out.println("Result = "+canTakePictures(a,b));
     }
 }
--- src/main/java/sorting/TrainingSessions.java
+++ src/main/java/sorting/TrainingSessions.java
@@ -1,5 +1,7 @@
 package sorting;
 
+import java.util.Arrays;
+
 /**
  * The Olympic Games organizers need to allocate facilities for the athletes' training sessions.
  * Each team has a schedule of training sessions with a start and end time
@@ -27,7 +29,7 @@ package sorting;
  *
  *
  * More formally, the goal is to minimize k such that for all time t,
- * the number of sessions that overlap at time t is at most k
+ * the number of sessions that overlap at time t is at most kx^x
  *
  * The computation must be performed in O(n.log(n)) time complexity
  * where n is the number of training sessions.
@@ -44,9 +46,80 @@ public class TrainingSessions {
      *                 a session with start time and end time.
      * @return the minimum number of facilities required.
      */
-    public int minFacilitiesRequired(int[][] sessions) {
+    /*public int minFacilitiesRequired(int[][] sessions) {
         // TODO
-         return -1;
+
+        sort(sessions);
+
+        return calculateMinimumFacilities(sessions);
     }
 
+    public static void main(String[] args) {
+        int[][] sessions = {{10,13},{8,15},{13,17},{14,16},{9,14},{17,22},{15,18},{19,22},{19,21}};
+        TrainingSessions sess = new TrainingSessions();
+        sess.minFacilitiesRequired(sessions);
+    }
+
+    public static int calculateMinimumFacilities(int[][] sessions){
+        int minimumNumberFacilities=1;
+        for (int i = 0; i < sessions.length; i++) {
+            int numberFacilitiesInUse=1;
+            for (int j=i+1;j<sessions.length;j++){
+                if(sessions[j][0]<sessions[i][1]){
+                    numberFacilitiesInUse++;
+                    if (numberFacilitiesInUse > minimumNumberFacilities) {
+                        minimumNumberFacilities = numberFacilitiesInUse;
+                    }
+                }
+                else{
+                    break;//ca veut dire que la session i est terminée vu que le début de j est plus grand ou égal que la fin de i
+                }
+            }
+        }
+        return minimumNumberFacilities;
+    }
+
+    public static void sort(int [][] sessions){
+        Arrays.sort(sessions, (a, b) -> {
+            if (a[0] != b[0])
+                return Integer.compare(a[0], b[0]);  // d'abord heure du début début
+            return Integer.compare(a[1], b[1]);       // ensuite heure de fin
+        });
+    }*/
+
+    public int minFacilitiesRequired(int[][] sessions) {
+        if (sessions.length == 0) {
+            return 0;
+        }
+
+        // Séparer les heures de début et de fin
+        int[] starts = new int[sessions.length];
+        int[] ends = new int[sessions.length];
+        for (int i = 0; i < sessions.length; i++) {
+            starts[i] = sessions[i][0];
+            ends[i] = sessions[i][1];
+        }
+        // Tri des tableaux
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+
+        int usedFacilities = 0;
+        int maxFacilities = 0;
+        int i = 0;
+        int j = 0;
+        while (i < sessions.length && j < sessions.length) {
+            // Une session commence avant qu'une autre se termine = on ajoute une salle
+            if (starts[i] < ends[j]) {
+                usedFacilities++;
+                maxFacilities = Math.max(maxFacilities, usedFacilities);
+                i++;
+            }
+            // Une session s'est terminée avant la suivante = on libère une salle
+            else {
+                usedFacilities--;
+                j++;
+            }
+        }
+        return maxFacilities;
+    }
 }
